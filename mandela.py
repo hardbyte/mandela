9ea1ed9dab577e98a07f3d694960a55cebdd2cb4
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2014-04-30 10:13:55 +1000

    add mandela.py

---
+++ mandela.py
@@ -0,0 +1,14 @@
+import strats
+
+
+def run_championship(strat_list):
+    pass
+
+def main():
+    possible_strats = [strats.AlwaysCooperate,
+                       strats.AlwaysDefect,
+                       strats.TitForTat]
+    run_championship(possible_strat)
+
+if __name__ == "__main__":
+    main()
