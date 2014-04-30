babbe7d7459e3c3fdadb0b12381c57afcf6fa8bb
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2014-04-30 10:40:16 +1000

    fixed bug

--- mandela.py
+++ mandela.py
@@ -21,18 +21,19 @@ def roundPoints(strat1, strat2):
 
 def run_championship(strat_list):
     nstrats = len(strat_list)
-    global_history = np.zeros((number_of_rounds, nstrats))
     pairings = list(pairs(xrange(nstrats)))
+    npairings = len(pairings)
+    global_history = np.zeros((number_of_rounds, npairings, 2))
     for round in xrange(number_of_rounds):
-        for p in pairings:
+        for i, p in enumerate(pairings):
             idx1 = p[0]
             idx2 = p[1]
-            hist1 = global_history[:, idx1]
-            hist2 = global_history[:, idx2]
+            hist1 = global_history[:, i, 0]
+            hist2 = global_history[:, i, 1]
             action1 = strat_list[idx1].determine_action(hist1, hist2)
             action2 = strat_list[idx2].determine_action(hist2, hist1)
-            global_history[round, idx1] = action1
-            global_history[round, idx2] = action2
+            global_history[round,i, 0] = action1
+            global_history[round,i, 0] = action2
     return global_history
 
 def main():
