a2b5679e946b5038a25d36d51bff9a75bb5f779e
Brian Thorne <hardbyte@gmail.com>
2014-04-30 11:27:49 +1000

    Label for strats

--- datatypes.py
+++ datatypes.py
@@ -8,7 +8,12 @@ class Strategy:
     takes_parameter = False
 
     def __repr__(self):
-        return "Strategy <{}>".format(self.__class__.__name__)
+        p = ""
+        if self.takes_parameter:
+            p = str(self.skew)
+            return "{} ({})".format(self.__class__.__name__, p)
+        else:
+            return self.__class__.__name__
 
     def determine_action(self, my_moves, their_moves, turn=0):
         """
